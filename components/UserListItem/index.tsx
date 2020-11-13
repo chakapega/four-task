e7c6329c32f30f04d5f6c618a72a08a8c7071349
Marchenko, Vadim3 <v3.marchenko@itransition.com>
2020-11-13 18:34:54 +0300

    added link to UserListItem

--- components/UserListItem/index.tsx
+++ components/UserListItem/index.tsx
@@ -1,8 +1,22 @@
 import { TableRow, TableCell, Avatar } from '@material-ui/core';
 import { UserListItemPropsType } from '../../types';
+import Link from 'next/link';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  tableRow: {
+    cursor: 'pointer',
+    '&:hover': {
+      background: '#cde6e6',
+    },
+  },
+});
 
 export default function UserListItem({ user }: UserListItemPropsType): JSX.Element {
+  const classes = useStyles();
+
   const {
+    login: { uuid },
     picture: { thumbnail },
     name: { first, last },
     dob: { date },
@@ -16,15 +30,17 @@ export default function UserListItem({ user }: UserListItemPropsType): JSX.Eleme
   } = user;
 
   return (
-    <TableRow>
-      <TableCell>
-        <Avatar alt={`${first} ${last}`} src={thumbnail} />
-      </TableCell>
-      <TableCell>{`${first} ${last}`}</TableCell>
-      <TableCell>{date}</TableCell>
-      <TableCell>{email}</TableCell>
-      <TableCell>{phone}</TableCell>
-      <TableCell>{`${country},${city},${name} ${number}`}</TableCell>
-    </TableRow>
+    <Link href={`/user/${uuid}`}>
+      <TableRow className={classes.tableRow}>
+        <TableCell>
+          <Avatar alt={`${first} ${last}`} src={thumbnail} />
+        </TableCell>
+        <TableCell>{`${first} ${last}`}</TableCell>
+        <TableCell>{date}</TableCell>
+        <TableCell>{email}</TableCell>
+        <TableCell>{phone}</TableCell>
+        <TableCell>{`${country},${city},${name} ${number}`}</TableCell>
+      </TableRow>
+    </Link>
   );
 }
